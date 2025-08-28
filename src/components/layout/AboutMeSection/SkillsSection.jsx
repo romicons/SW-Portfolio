78e6c4abd627f364981d8842feb9e5b51368b5ff
romicons <romina.constantinoff@gmail.com>
2025-08-28 14:54:35 -0300

    Modified the layout of the AboutMe Section.

--- src/components/layout/AboutMeSection/SkillsSection.jsx
+++ src/components/layout/AboutMeSection/SkillsSection.jsx
@@ -1,19 +1,32 @@
 import "./skillsSection.css"
-
 import { SkillCard } from "./SkillCard"
 
 export const SkillsSection = () => {
-    return(
+    return (
         <div className="skills__cards__container">
-            <SkillCard tittle="Bootstrap" icon="devicon-bootstrap-plain" hoverColor='#6B21A8'/>
-            <SkillCard tittle="CSS 3" icon="devicon-css3-plain" hoverColor='#1E40AF' />
-            <SkillCard tittle="GIT" icon="devicon-git-plain" hoverColor='#14532D'/>
+
+            {/* Lenguajes base */}
             <SkillCard tittle="HTML 5" icon="devicon-html5-plain" hoverColor='#F97316' />
+            <SkillCard tittle="CSS 3" icon="devicon-css3-plain" hoverColor='#1E40AF' />
             <SkillCard tittle="Javascript" icon="devicon-javascript-plain" hoverColor='#EAB308'/>
-            <SkillCard tittle="React" icon="devicon-react-original" hoverColor='#2563EB' />
-            <SkillCard tittle="React Bootstrap" icon="devicon-reactbootstrap-original" hoverColor='#3B82F6' />
+            <SkillCard tittle="TypeScript" icon="devicon-typescript-plain" hoverColor='#3178c6' />
+
+            {/* Preprocesadores y frameworks de estilos */}
             <SkillCard tittle="Sass" icon="devicon-sass-original" hoverColor='#EC4899' />
-            <SkillCard tittle="Tailwind Css" icon="devicon-tailwindcss-original" hoverColor='#60A5FA' />
+            <SkillCard tittle="Bootstrap" icon="devicon-bootstrap-plain" hoverColor='#6B21A8'/>
+            <SkillCard tittle="Tailwind CSS" icon="devicon-tailwindcss-original" hoverColor='#60A5FA' />
+            <SkillCard tittle="Chakra UI" icon="devicon-chakraui-plain" hoverColor='#259e94' />
+            <SkillCard tittle="Material UI" icon="devicon-materialui-plain" hoverColor='#0073e6' />
+            <SkillCard tittle="React Bootstrap" icon="devicon-reactbootstrap-original" hoverColor='#3B82F6' />
+
+            {/* Frameworks / Librerías */}
+            <SkillCard tittle="React" icon="devicon-react-original" hoverColor='#2563EB' />
+            <SkillCard tittle="Node Js" icon="devicon-nodejs-plain" hoverColor='#2c682c' />
+
+            {/* Herramientas */}
+            <SkillCard tittle="Git" icon="devicon-git-plain" hoverColor='#f05133' />
+            <SkillCard tittle="NPM" icon="devicon-npm-plain" hoverColor='#cd3e3d' />
+
         </div>
     )
 }
--- src/components/layout/ProjectsSection/ProjectsSection.jsx
+++ src/components/layout/ProjectsSection/ProjectsSection.jsx
@@ -7,7 +7,7 @@ export const ProjectsSection = () => {
     return(
         <section className="projects__section">
             <h2 className="section__title shadow-yellow">Projects</h2>
-            <div className="options flex flex-row items-stretch overflow-hidden min-w-[600px] max-w-[900px] h-[400px] calc-width">
+            <div className="options">
                 <ProjectCard icon={<GoTDataIcon />}  projectName={'GoT Data'} description={'Utilizing MockAPI services and consuming specifically generated data, this web application enables users to perform basic CRUD (Create, Read, Update, and Delete) functions. With impeccable and eye-catching design, it invites enthusiasts to expand the API through an aesthetic and interactive experience.'} />
             </div>
         </section>
